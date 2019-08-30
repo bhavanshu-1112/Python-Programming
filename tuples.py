13761760364037b7c07043746f267e06ada6fdad
Bhavanshu Jain <46085014+bhavanshu-1112@users.noreply.github.com>
2019-08-30 19:08:55 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,15 @@
+# tuples
+# used when you need to complete the task in short time (tuples has less execution time)
+# used where you have to deal with values which cannot be changed
+tup = ("python","android","java",29782)
+print len(tup)
+print max(tup)
+print min(tup)
+
+# adding of tuples
+tup2 = (1,2,3,4)
+tup3 = tup + tup2
+print tup3
+del tup2
+print tup3
+# print tup2
